0c1b1e94abe04b34a479963883f15c94b7e95f5f
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-08-14 12:03:15 -0500

    project finalized

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const catchRequests = async (req) => {
+    const response = await fetch(req)
+    const data = await response.json()
+    
+
+    const js = (rep) => rep.language == "JavaScript"
+    
+    let justJs = data.filter(js)
+    let justNames = []
+    for (let i in justJs){
+        justNames.push(justJs[i].name)
+    }
+    document.getElementById("repos").textContent= justNames
+    //console.log(data)
+    //console.log(justJs)
+    
+        
+    
+
+}
+
+const search = () =>{
+    const api_url = "https://api.github.com/users/"
+        
+    let user = document.getElementById("MyInput").value
+        
+    let req = new Request(`${api_url}${user}/repos`)
+
+    catchRequests(req).catch(() => console.log("Algo salio mal"))
+}
